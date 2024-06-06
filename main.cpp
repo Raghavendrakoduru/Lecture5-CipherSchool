e18dea3c900c7acdf6bb6fae891649d312c1688f
Raghavendra Koduru <119060193+Raghavendrakoduru@users.noreply.github.com>
2024-06-06 11:40:35 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //conditional Statements
+    int a,b,c,d;
+    cout<<"Input two integers";
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+    cout<<(a>b);
+    cout<<(a>b && c>d);
+    retuno 0;
+
+}
